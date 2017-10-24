bb54294f5b53c4c3879cf05c65a7d204193bc2ef
hunghv2 <hunghv2@outlook.com>
2017-10-24 15:26:44 +0700

    add new data

--- WebAPI/WebAPI.Data/Properties/AssemblyInfo.cs
+++ WebAPI/WebAPI.Data/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Data")]
+[assembly: AssemblyTitle("WebAPI.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Data")]
+[assembly: AssemblyProduct("WebAPI.Data")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4a2ce4ff-809e-49f2-851b-98ea581a514e")]
+[assembly: Guid("056ee8c7-bc0c-495b-a77b-f1ad75870037")]
 
 // Version information for an assembly consists of the following four values:
 //
